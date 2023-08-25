3ab0340e94bff4abc09d8254535696f104f51418
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-08-24 22:24:54 -0500

    Added React Router.
    Also created some pages/routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+
 function App() {
-	return <div>WorldWise</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="*"
+					element={<PageNotFound />}
+				/>
+				<Route
+					path="pricing"
+					element={<Pricing />}
+				/>
+				<Route
+					path="product"
+					element={<Product />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
