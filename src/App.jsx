617b623439fffc7f83eafe4460e6596b0faa1d2b
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-08-27 23:46:06 -0500

    Learned about the Navigation component.
    This is the declarative way of using React Navigation. :0

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import AppLayout from "./pages/AppLayout";
 import City from "./components/City";
@@ -55,9 +55,9 @@ function App() {
 					<Route
 						index
 						element={
-							<CityList
-								cities={cities}
-								isLoading={isLoading}
+							<Navigate
+								replace
+								to="cities"
 							/>
 						}
 					/>
