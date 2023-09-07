dbd4915c210171869c9b9be2dc9d9c990696b700
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-09-06 20:15:16 -0500

    Learned about optimizing bundle size by splitting code
    This is a really cool feature! I also fixed a typo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,94 @@
+import { Suspense, lazy } from "react";
+
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import AppLayout from "./pages/AppLayout";
+// import AppLayout from "./pages/AppLayout";
+// import Homepage from "./pages/Homepage";
+// import Login from "./pages/Login";
+// import PageNotFound from "./pages/PageNotFound";
+// import Pricing from "./pages/Pricing";
+// import Product from "./pages/Product";
+
 import City from "./components/City";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
-import Homepage from "./pages/Homepage";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
+
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Login = lazy(() => import("./pages/Login"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
 
 function App() {
 	return (
 		<AuthProvider>
 			<CitiesProvider>
 				<BrowserRouter>
-					<Routes>
-						<Route
-							path="/"
-							element={<Homepage />}
-						/>
-						<Route
-							path="*"
-							element={<PageNotFound />}
-						/>
-						<Route
-							path="app"
-							element={
-								<ProtectedRoute>
-									<AppLayout />
-								</ProtectedRoute>
-							}
-						>
+					<Suspense fallback={<SpinnerFullPage />}>
+						<Routes>
 							<Route
-								index
+								path="/"
+								element={<Homepage />}
+							/>
+							<Route
+								path="*"
+								element={<PageNotFound />}
+							/>
+							<Route
+								path="app"
 								element={
-									<Navigate
-										replace
-										to="cities"
-									/>
+									<ProtectedRoute>
+										<AppLayout />
+									</ProtectedRoute>
 								}
+							>
+								<Route
+									index
+									element={
+										<Navigate
+											replace
+											to="cities"
+										/>
+									}
+								/>
+								<Route
+									path="cities"
+									element={<CityList />}
+								/>
+								<Route
+									path="cities/:id"
+									element={<City />}
+								/>
+								<Route
+									path="countries"
+									element={<CountryList />}
+								/>
+								<Route
+									path="form"
+									element={<Form />}
+								/>
+							</Route>
+							<Route
+								path="login"
+								element={<Login />}
 							/>
 							<Route
-								path="cities"
-								element={<CityList />}
+								path="pricing"
+								element={<Pricing />}
 							/>
 							<Route
-								path="cities/:id"
-								element={<City />}
+								path="product"
+								element={<Product />}
 							/>
-							<Route
-								path="countries"
-								element={<CountryList />}
-							/>
-							<Route
-								path="form"
-								element={<Form />}
-							/>
-						</Route>
-						<Route
-							path="login"
-							element={<Login />}
-						/>
-						<Route
-							path="pricing"
-							element={<Pricing />}
-						/>
-						<Route
-							path="product"
-							element={<Product />}
-						/>
-					</Routes>
+						</Routes>
+					</Suspense>
 				</BrowserRouter>
 			</CitiesProvider>
 		</AuthProvider>
